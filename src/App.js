7bbb8fc963d63c302e01960d1befac126f52600a
DiyorbekXudoberganov85 <xudaberganovdiyorbek556@gmail.com>
2026-01-22 16:58:14 +0500

    yangi

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import {Route , Routes} from 'react-router-dom';
+import { Main  , Login ,Register} from './components';
+const App = () => {
+  return (
+    <div>
+     <Routes>
+      <Route path='/' element={<Main />} />
+      <Route path='/login' element={<Login />} />
+      <Route path='/register' element={<Register />} />
+     </Routes>
+    </div>
+  )
+}
+
+export default App
