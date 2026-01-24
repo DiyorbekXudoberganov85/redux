e18d8d14629fc9612bb7a0f8d26a2ab2406ceef7
DiyorbekXudoberganov85 <xudaberganovdiyorbek556@gmail.com>
2026-01-24 18:17:02 +0500

    9-dars redux bilan ishlash

--- src/components/login.jsx
+++ src/components/login.jsx
@@ -1,18 +1,29 @@
 import { useState } from 'react'
 import {icon} from '../constant'
 import { Input } from '../ui'
+import { useSelector, useDispatch } from 'react-redux'
+import { loginUserStart } from '../slice/auth'
 const Login = () => {
   const [email, setEmail] = useState('')
   const [password, setPassword] = useState('')
+  
+  const dispatch = useDispatch()  
+  const {isLoading} = useSelector(state => state.auth)
+  console.log(isLoading)
+  const loginHendler = (e) => {
+  e.preventDefault();
+  dispatch(loginUserStart())
+}
+
   return (
     <div className="text-center mt-5"> 
      <main className="form-signin w-25 m-auto"> 
       <form> 
         <img className="mb-4" src={icon} alt="" width="95" height="95"/> 
-        <h1 class="h3 mb-3 fw-normal">Please login</h1> 
+        <h1 className="h3 mb-3 fw-normal">Please login</h1> 
         <Input label='Email address' state={email} setState={setEmail}/>
         <Input label='Password' type='password' state={password} setState={setPassword}/>
-               <button class="btn btn-primary w-100 py-2 mt-2"  type="submit">Sign in</button> 
+        <button className="btn btn-primary w-100 py-2 mt-2" disabled={isLoading} onClick={loginHendler} type="submit">{isLoading ? 'Loading...' : 'Login'}</button> 
            
                </form> 
                </main>
--- src/components/register.jsx
+++ src/components/register.jsx
@@ -11,18 +11,18 @@ const Register = () => {
      <main className="form-signin w-25 m-auto"> 
       <form> 
         <img className="mb-4" src={icon} alt="" width="95" height="95"/> 
-        <h1 class="h3 mb-3 fw-normal">Please register</h1> 
+        <h1 className="h3 mb-3 fw-normal">Please register</h1> 
         <Input label='Username' state={name} setState={setname}/>
         <Input label='Email address' state={email} setState={setEmail}/>
         <Input label='Password' type='password' state={password} setState={setPassword}/>
               
             
-               <button class="btn btn-primary w-100 py-2 mt-2"  type="submit">Sign in</button> 
+        <button className="btn btn-primary w-100 py-2 mt-2"  type="submit">Registr</button> 
            
                </form> 
                </main>
     </div>
   )
-}
+} 
 
 export default Register
